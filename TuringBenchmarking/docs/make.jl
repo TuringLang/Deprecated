3f55f3c2801110293b7ee8d0f70b491c8bddf270
Shravan Goswami <shravanngoswamii@gmail.com>
2025-05-05 15:40:29 +0530

    TuringBenchmarking: Updating URLs

--- TuringBenchmarking/docs/make.jl
+++ TuringBenchmarking/docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(TuringBenchmarking, :DocTestSetup, :(using TuringBenchmarkin
 makedocs(;
     modules=[TuringBenchmarking],
     authors="Tor Erlend Fjelde <torfjelde.github@gmail.com> and contributors",
-    repo="https://github.com/TuringLang/depreciated/blob/{commit}{path}#{line}",
+    repo="https://github.com/TuringLang/Deprecated/blob/{commit}{path}#{line}",
     sitename="TuringBenchmarking.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://github.com/TuringLang/depreciated",
+        canonical="https://github.com/TuringLang/Deprecated",
         edit_link="main",
         assets=String[],
     ),
